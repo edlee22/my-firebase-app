2ca9c9429604d78ac4fae219c1f1cfecf197ee5c
Edmund Lee <edmundleekc@gmail.com>
2022-06-10 21:53:02 -0600

    Added FireStore Realtime Listeners

--- src/components/HeroesList.jsx
+++ src/components/HeroesList.jsx
@@ -1,5 +1,11 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
-import React, { useContext, useState } from "react";
+import {
+  collection,
+  getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
 import { FirebaseContext } from "../providers/FirebaseProvider";
 
 function HeroesList() {
@@ -8,27 +14,44 @@ function HeroesList() {
 
   const [heroes, setHeroes] = useState([]);
 
-  const getHeroesData = async () => {
-    try {
-      let collectionRef = collection(db, "heroes");
-      let queryRef = query(collectionRef, orderBy("name", "asc"));
-      let querySnap = await getDocs(queryRef);
+  // const getHeroesData = async () => {
+  //   try {
+  //     let collectionRef = collection(db, "heroes");
+  //     let queryRef = query(collectionRef, orderBy("name", "asc"));
+  //     let querySnap = await getDocs(queryRef);
+  //     if (querySnap.empty) {
+  //       console.log("No docs found");
+  //     } else {
+  //       let heroesData = querySnap.docs.map((doc) => ({
+  //         ...doc.data(),
+  //         DOC_ID: doc.id,
+  //       }));
+  //       setHeroes(heroesData);
+  //     }
+  //   } catch (ex) {
+  //     console.log("FIRESTORE FAILURE!", ex.message);
+  //   }
+  // };
+
+  // Firestore Realtime Listeners!
+  useEffect(() => {
+    let collectionRef = collection(db, "heroes");
+    let queryRef = query(collectionRef, orderBy("name"));
+    const unsubscribe = onSnapshot(queryRef, (querySnap) => {
       if (querySnap.empty) {
         console.log("No docs found");
       } else {
-        let heroesData = querySnap.docs.map((doc) => ({
-          ...doc.data(),
-          DOC_ID: doc.id,
-        }));
+        let heroesData = querySnap.docs.map((doc) => doc.data());
         setHeroes(heroesData);
       }
-    } catch (ex) {
-      console.log("FIRESTORE FAILURE!", ex.message);
-    }
-  };
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <div>
-      <button onClick={() => getHeroesData()}>GET DATA</button>
+      {/* When using Firestore Realtime Listeners - get rid of getHeroesData
+      <button onClick={() => getHeroesData()}>GET DATA</button> */}
       <br />
       {heroes.map((hero) => {
         return (
